7003c393a21d4157abdf8cd2fad90e8e1b2addaf
YaChengMu <1140049363@qq.com>
2024-05-28 16:57:11 +0800

    20230528

--- src/apps/com.wifitutu.ts
+++ src/apps/com.wifitutu.ts
@@ -8,15 +8,25 @@ export default defineGkdApp({
       key: 1,
       name: '局部广告-首页-WiFi列表广告',
       activityIds: ['com.wifitutu.ui.main.MainActivity'],
+      quickFind: true,
       rules: [
         {
+          key: 0,
           matches: 'View < [desc^="dislike"][visibleToUser=true]',
           snapshotUrls: 'https://i.gkd.li/i/13233916',
         },
         {
+          key: 1,
           matches:
             '@Image[text=""] < View +n View > View > TextView[text="广告"]',
           snapshotUrls: 'https://i.gkd.li/i/13234048',
+        },
+        {
+          preKeys: [0, 1],
+          actionDelay: 1000,
+          key: 99,
+          matches: '[text="不感兴趣"] < @LinearLayout',
+          snapshotUrls: ['https://i.gkd.li/i/15466659'],
         },
       ],
     },
